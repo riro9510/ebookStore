b9c5d82dfa9845205c04cf7481a47cfa7c82b1ce
blessingmad <mad22008@byui.edu>
2025-04-04 21:26:26 +0200

    initial push

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,8 +1,14 @@
 const router = require('express').Router()
 const booksController = require('../controllers/booksController')
 
+// route to get all books.
+router.get('/', booksController.getAll);
+
+router.get('/:id', booksController.getSingle); // route to get a books by id.
+
 router.post('/bulk', booksController.insertMultipleBooks)
 
 router.put('/update/:id', booksController.updateBook)
 
+router.delete('/:id', booksController.deleteOne)
 module.exports = router

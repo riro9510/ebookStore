6e3989127989fc5932fd51f6422f38f3feb78d23
Saberblaze22 <Comiccooldylan@gmail.com>
2025-04-04 07:15:43 -0600

    added more endpoints to books.js

--- src/routes/books.js
+++ src/routes/books.js
@@ -2,5 +2,11 @@ const router = require('express').Router()
 const booksController = require('../controllers/booksController')
 
 router.post('/bulk', booksController.insertMultipleBooks)
-
+router.post('/', booksController.insertBook)
+router.get('/', booksController.getAllBooks)
+router.get('/:id', booksController.getSingleBook)
+router.put('/:id', booksController.updateBook)
+router.delete('/:id', booksController.deleteBook)
+router.get('/search', booksController.searchBooks)
+router.patch('/:id', booksController.updateBookPartial)
 module.exports = router

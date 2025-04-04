cff1ee50529329cf02c8a7f5aa4a4af7810e613a
bcadolf <adolfbrandon96@gmail.com>
2025-04-04 12:44:47 -0400

    missing id field param

--- src/routes/books.js
+++ src/routes/books.js
@@ -3,6 +3,6 @@ const booksController = require('../controllers/booksController')
 
 router.post('/bulk', booksController.insertMultipleBooks)
 
-router.put('/update', booksController.updateBook)
+router.put('/update/:id', booksController.updateBook)
 
 module.exports = router

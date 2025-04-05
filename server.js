d5f0249d0ef6986f81b639730e4e2c46bfa172e1
Steven Thomas <stevencthomas54@gmail.com>
2025-04-05 10:53:09 -0800

    a

--- server.js
+++ server.js
@@ -7,38 +7,38 @@ Purpose: Startup script for the project
 // ==============================================
 // Section: Require statements
 // ===============================================
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 3000
-const cors = require('cors')
-const database = require('./src/database/index.js')
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+const database = require('./src/database/index.js');
 //const passport = require('passport');
 //const session = require('express-session');
 //const GitHubStrategy = require('passport-github2').Strategy;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 /*app.use(session({
   secret:"secret",
   resave: false,
   saveUninitialized:true,
 }))
 app.use(passport.initialize()).use(passport.session());*/
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  )
+  );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, PUT, DELETE, OPTIONS'
-  )
-  next()
-})
+  );
+  next();
+});
 /*passport.use(new GitHubStrategy({
   clientID:process.env.GITHUB_CLIENT_ID,
   clientSecret:process.env.GITHUB_CLIENT_SECRET,
@@ -63,8 +63,8 @@ passport.deserializeUser((user,done)=>{
 // app.set("layout", "./layouts/layout")
 
 // Allow the use of the static folder
-app.use(express.static('public'))
-app.use('/', require('./src/routes/index.js'))
+app.use(express.static('public'));
+app.use('/', require('./src/routes/index.js'));
 
-app.listen(port)
-console.log(`🚀Web server listening on port ${port}🚀`)
+app.listen(port);
+console.log(`🚀Web server listening on port ${port}🚀`);

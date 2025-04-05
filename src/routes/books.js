52a31e03faca4e9085e8dab71c713cdb7c265d53
Steven Thomas <stevencthomas54@gmail.com>
2025-04-05 15:32:14 -0800

    There was, uh, some problems.

--- src/routes/books.js
+++ src/routes/books.js
@@ -1,19 +1,14 @@
 const router = require('express').Router();
 const booksController = require('../controllers/booksController');
 
-// route to get all books.
+router.post('/bulk', booksController.insertMultipleBooks);
+// router.post('/', booksController.insertBook);
+
+router.put('/:id', booksController.updateBook);
+router.get('/:id', booksController.getSingleBook);
+router.delete('/:id', booksController.deleteBook);
+router.get('/update', booksController.buildBooksForm);
+router.post('/update/:id', booksController.updateBook);
 router.get('/', booksController.getAllBooks);
 
-router.get('/:id', booksController.getSingle); // route to get a books by id.
-
-router.post('/bulk', booksController.insertMultipleBooks)
-router.post('/', booksController.insertBook)
-router.get('/', booksController.getAllBooks)
-router.get('/:id', booksController.getSingleBook)
-router.put('/:id', booksController.updateBook)
-router.delete('/:id', booksController.deleteBook)
-router.get('/search', booksController.searchBooks)
-router.patch('/:id', booksController.updateBookPartial)
-router.get('/update', booksController.buildBooksForm)
-router.post('/update/:id', booksController.updateBook)
-module.exports = router
+module.exports = router;

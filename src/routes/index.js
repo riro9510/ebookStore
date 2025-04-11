f75765ff153922c5196e58dc10ba6f7d9c316fb1
Steven Thomas <stevencthomas54@gmail.com>
2025-04-11 12:53:15 -0800

    Fixed auth (probably, maybe)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,32 @@
 const router = require('express').Router();
-const passport = require('../config/passport.js');
-const { githubCallback } = require('../controller/authController');
-router.use('/', require('./swagger'));
 
+// Mount routes
+router.use('/', require('./swagger'));
 router.use('/books', require('./books'));
 router.use('/users', require('./users'));
-router.get('/login', passport.authenticate('github'), (req, res) => {});
+router.use('/auth', require('./auth'));
 
-router.get(
-  '/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  githubCallback
-);
+// Login page
+router.get('/login', (req, res) => {
+  res.send('<a href="/auth/github">Login With Github</a>');
+});
 
+// Home page
+router.get('/', (req, res) => {
+  console.log('user:', req.user);
+  console.log('session:', req.session);
+  const loggedStatus =
+    req.user !== undefined
+      ? `Logged in as ${req.user.displayName}`
+      : 'Logged out';
+
+  res.send(`
+    <p>${loggedStatus}</p>
+    <a href="/api-docs/">Click here to go to the API documentation</a>
+  `);
+});
+
+// Logout
 router.get('/logout', function (req, res, next) {
   req.logOut(function (err) {
     if (err) {
@@ -20,17 +34,6 @@ router.get('/logout', function (req, res, next) {
     }
     res.redirect('/');
   });
-});
-router.get('/', (req, res) => {
-  const loggedStatus =
-    req.session.user !== undefined
-      ? `Logged in as ${req.session.user.displayName}`
-      : 'Logged out';
-
-  res.send(`
-    <p>${loggedStatus}</p>
-    <a href="/api-docs/">Click here to go to the API documentation</a>
-  `);
 });
 
 module.exports = router;

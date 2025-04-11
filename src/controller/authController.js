75f942957d300c27fdd5679882985f76ffc488f5
ricardoingeaa <115902704+ricardoingeaa@users.noreply.github.com>
2025-04-10 23:21:41 -0600

    corrigiendo nuevamente el callback

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,8 +1,7 @@
-const jwt = require("../config/jwt");
 
 function githubCallback(req, res) {
   if (!req.user) {
-    return res.status(401).json({ message: "No Authorized" });
+    return res.status(401).json({ message: "No autorizado" });
   }
 
   req.login(req.user, (err) => {
@@ -10,7 +9,8 @@ function githubCallback(req, res) {
       return res.status(500).json({ message: "Error al iniciar sesión" });
     }
 
-    res.json({ message: "Autenticado correctamente" });
+   
+    res.redirect('/'); 
   });
 }
 
